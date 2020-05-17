63220ecdd185edee5a2d1d8a641c7a96ed0d7104
Dan Ellis <daniel.ellis.research@gmail.com>
2020-05-17 17:52:37 +0100

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,17 +1,14 @@
-sudo -s 
-cd /root
+sudo apt-get -y update;
+sudo apt-get -y upgrade;
 
-sudo apt-get update
-sudo apt-get upgrade
-
-sudo apt-get install -y git python3-pip
-sudo apt-get install -y python-serial python3-serial
-sudo apt-get install -y python-dev python3-dev
-sudo apt-get install -y python-spidev python3-spidev
-sudo apt-get install -y python-smbus
-sudo apt-get install -y i2c-tools
+sudo apt-get install -y git python3-pip;
+sudo apt-get install -y python-serial python3-serial;
+sudo apt-get install -y python-dev python3-dev;
+sudo apt-get install -y python-spidev python3-spidev;
+sudo apt-get install -y python-smbus;
+sudo apt-get install -y i2c-tools;
 
 
 
 
-sudo apt-get install bluez python-bluez
+#sudo apt-get install bluez python-bluez;
