069b0916b7ae20804b290d131bb2180634923efe
Dan Ellis <daniel.ellis.research@gmail.com>
2020-05-17 16:48:33 +0100

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -8,6 +8,8 @@ sudo apt-get install -y git python3-pip
 sudo apt-get install -y python-serial
 sudo apt-get install -y python-dev python3-dev
 sudo apt-get install -y python-spidev python3-spidev
+sudo apt-get install -y python-smbus
+sudo apt-get install -y i2c-tools
 
 
 
