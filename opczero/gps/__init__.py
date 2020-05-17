63c8030b147da887700e7eba1a59eb089c45dd44
Dan Ellis <daniel.ellis.research@gmail.com>
2020-05-17 17:11:06 +0100

    Update __init__.py

--- opczero/gps/__init__.py
+++ opczero/gps/__init__.py
@@ -13,7 +13,9 @@ def connect():
     global ser
     for i in range(10):
         try:# each unplug registers as a new number, we dont expect more than 10 unplugs without a restart
-            ser = serial.Serial('/dev/ttyACM%d'%i)
+            try:ser = serial.Serial('/dev/ttyAMA%d'%i)
+            except: ser = serial.Serial('/dev/ttyACM%d'%i)
+            
             print( 'Connected serial on /dev/ttyACM%d'%i)
             break
         except:continue
