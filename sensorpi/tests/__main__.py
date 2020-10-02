e144bd2852c0fcd18fa27ed2e35c79edf357281a
Dan Ellis <d.ellis@leeds.ac.uk>
2020-10-02 21:24:04 +0100

    Update __main__.py

--- sensorpi/tests/__main__.py
+++ sensorpi/tests/__main__.py
@@ -8,12 +8,12 @@ print('This is pi of serial number: ',os.popen('cat /sys/firmware/devicetree/bas
 
 
 
-# from . import pyvers
-# from . import interrupt_test
-# from . import db_test
+from . import pyvers
+from . import interrupt_test
+from . import db_test
 # 
 from . import DHT_test
-# from . import opc_test
+from . import opc_test
 from . import gps_test
 
 
