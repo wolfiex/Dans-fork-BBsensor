afcfa7bbfca3bac28869f8d5f410a622a831e1a0
cemac-ccs <c.c.symonds@leeds.ac.uk>
2020-10-28 22:01:36 +0000

    modified server db module to match sensor db

--- serverpi/db/__main__.py
+++ serverpi/db/__main__.py
@@ -27,15 +27,15 @@ CREATE TABLE MEASUREMENTS
           SERIAL       CHAR(16)    NOT NULL,
           TYPE         INT         NOT NULL,
           TIME         CHAR(6)     NOT NULL,
-          DATE         CHAR(8)     NOT NULL,
           LOC          BLOB        NOT NULL,
           PM1          REAL        NOT NULL,
           PM3          REAL        NOT NULL,
           PM10         REAL        NOT NULL,
+          T            REAL        NOT NULL,
+          RH           REAL        NOT NULL,
           SP           REAL        NOT NULL,
-          RC           INT        NOT NULL,
-          NSAT         INT
-);
+          RC           INT         NOT NULL,
+          UNIXTIME     INT         NOT NULL);
 ''')
 
 conn.execute('''
