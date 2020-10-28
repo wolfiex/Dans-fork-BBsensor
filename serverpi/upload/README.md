edf75d8c95b8dc35116703266a5a890007b8c0b6
cemac-ccs <c.c.symonds@leeds.ac.uk>
2020-10-28 19:11:46 +0000

    Added upload scaffolding to server branch

---
+++ serverpi/upload/README.md
@@ -0,0 +1,14 @@
+## Update to contain external internet credentials
+
+
+`sudo nano /etc/wpa_supplicant/wpa_supplicant.conf`
+
+And enter your details
+
+
+```
+network={
+    ssid="HOME NETWORK NAME"
+    psk="HOME PASSWORD"
+}
+```
---
+++ serverpi/upload/__init__.py
@@ -0,0 +1,25 @@
+'''
+scripts to run if connected online
+'''
+import os
+
+def online():
+
+    cmd = '''
+    PINGS=2
+    TESTIP=8.8.8.8
+    if ( ping -c $PINGS $TESTIP > /dev/null ) then
+        echo "1"
+    else
+        echo "0"
+    fi
+    '''
+
+    return int(os.popen(cmd).read())
+
+
+
+def sync():
+
+    # so something
+    return True
