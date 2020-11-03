f64ab8d50a7a69f6cab0d0bb4e210d2823e9f785
cemac-ccs <c.c.symonds@leeds.ac.uk>
2020-11-03 11:33:10 +0000

    test to show tables when resetting db

--- serverpi/db/builddb.py
+++ serverpi/db/builddb.py
@@ -2,6 +2,18 @@ import os,sys,sqlite3
 
 def builddb(conn):
 
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+    table_counter = 0
+    print("SQL Tables available: \n===================================================\n")
+    for table_item in cursor_a.fetchall():
+        current_table = table_item[0]
+        table_counter += 1
+        print("-> " + current_table)
+    print("\n===================================================\n")
+
     #ID INTEGER PRIMARY KEY AUTOINCREMENT,
     conn.execute('''
                  CREATE TABLE MEASUREMENTS
@@ -32,3 +44,13 @@ def builddb(conn):
 
 
     conn.commit()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+    table_counter = 0
+    print("SQL Tables available: \n===================================================\n")
+    for table_item in cursor_a.fetchall():
+        current_table = table_item[0]
+        table_counter += 1
+        print("-> " + current_table)
+    print("\n===================================================\n")
