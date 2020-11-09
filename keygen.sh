741a58d3c61e8a487b875bf2cc93edd35d932ecd
cemac-ccs <c.c.symonds@leeds.ac.uk>
2020-11-09 17:07:29 +0000

    edited keygen script

--- keygen.sh
+++ keygen.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-str=$( curl -s https://raw.githubusercontent.com/first20hours/google-10000-english/master/20k.txt | perl -MList::Util=shuffle -e 'print shuffle<STDIN>' | sed -E 's/\b\(\w\)\{,4\}\b\s*//g' | head -n4 | tr '\n' ' ')
+str=$( curl -s https://raw.githubusercontent.com/first20hours/google-10000-english/master/20k.txt | perl -MList::Util=shuffle -e 'print shuffle<STDIN>' | head | tr '\n' ' ' | sed -E 's/\b\(\w\)\{,4\}\b\s*//g' | awk '{print $1" "$2" "$3" "$4;}' )
 sudo echo "serverpi_access_key = $str" > /root/.serverpi
-ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -N $str
+ssh-keygen -q -t rsa -N "${str}" -f /root/.ssh/id_rsa
 ssh-copy-id -i /root/.ssh/id_rsa serverpi@10.3.141.1
